c4e93bd8bcb218f58d6c00cee5b514715257a70b
SathsaraniBS <sandunibbs@gmail.com>
2026-07-15 23:09:36 +0530

    fix(Navbar): update import paths for Navbar component and Button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
