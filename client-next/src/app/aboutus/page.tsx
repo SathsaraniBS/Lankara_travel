d03c69373c1bee4a63e82c1712da451287e6b4fb
SathsaraniBS <sandunibbs@gmail.com>
2026-08-12 22:49:51 +0530

    feat: update HomePage UI with improved text color for better visibility

---
+++ client-next/src/app/aboutus/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import api from "@/lib/api";
+
+export default function AboutUspage() {
+ 
+ 
+  return (
+    <div className="min-h-screen bg-gray-50">
+      {/* Search Header */}
+      <div className="bg-gradient-to-br from-orange-50 to-cyan-50 py-12 px-6">
+        <div className="max-w-5xl mx-auto">
+          <h1 className="text-3xl font-bold text-gray-900 mb-6">
+            About Us 
+          </h1>
+
+        
+
+       
+          </div>
+        
+      </div>
+    </div>
+  );
+}
